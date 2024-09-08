596f3745882621911dc7a3e844fca763a3fa96c6
ironkentank <86238798+ironkentank@users.noreply.github.com>
2024-09-08 23:44:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ from page2 import run as page2_run
 from page3 import run as page3_run
 from page4 import run as page4_run
 
+# inisialiasi nltk dan nlp untuk analisis sentimen dari pipeline
 nltk.download('punkt')
+nltk.download('stopwords')
+nlp = pipeline('sentiment-analysis')
 
 # Ensure the 'punkt' tokenizer data is downloaded
 def ensure_nltk_data():
